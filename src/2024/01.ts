0e2ba47fa5370652b71698bdbe9e39b4378d0e03
Sebastian Heinz <sebastian@iamsebastian.dev>
2024-12-01 21:14:01 +0100

    refactor: change statistics implementation

--- src/2024/01.ts
+++ src/2024/01.ts
@@ -1,52 +1,52 @@
-import { getLines } from "../core/file/get-lines";
-import { getDistance } from "../core/numbers/get-distance";
-import { Run } from "../core/run";
+import { getLines } from '../core/file/get-lines';
+import { getDistance } from '../core/numbers/get-distance';
+import { Run } from '../core/run';
 
 export default class extends Run {
-    private processFile(file: string) {
-        // we need to split and map the input
-        // into two different arrays, that can then
-        // be sorted and evaluated
-        const lines = getLines(file);
-        const leftNumbers: number[] = [];
-        const rightNumbers: number[] = [];
+	private processFile(file: string) {
+		// we need to split and map the input
+		// into two different arrays, that can then
+		// be sorted and evaluated
+		const lines = getLines(file);
+		const leftNumbers: number[] = [];
+		const rightNumbers: number[] = [];
 
-        for (const line of lines) {
-            const [left, right] = line.split("   ");
-            leftNumbers.push(Number.parseInt(left));
-            rightNumbers.push(Number.parseInt(right));
-        }
+		for (const line of lines) {
+			const [left, right] = line.split('   ');
+			leftNumbers.push(Number.parseInt(left));
+			rightNumbers.push(Number.parseInt(right));
+		}
 
-        return [leftNumbers, rightNumbers];
-    }
+		return [leftNumbers, rightNumbers];
+	}
 
-    solvedPuzzleOne = true;
-    getSolutionOne(file: string): string {
-        const [left, right] = this.processFile(file);
+	solvedPuzzleOne = true;
+	getSolutionOne(file: string): string {
+		const [left, right] = this.processFile(file);
 
-        const leftSorted = left.sort();
-        const rightSorted = right.sort();
-        const distances: number[] = [];
+		const leftSorted = left.sort();
+		const rightSorted = right.sort();
+		const distances: number[] = [];
 
-        for (let i = 0; i < leftSorted.length; i++) {
-            distances.push(getDistance(leftSorted[i], rightSorted[i]));
-        }
+		for (let i = 0; i < leftSorted.length; i++) {
+			distances.push(getDistance(leftSorted[i], rightSorted[i]));
+		}
 
-        return distances.reduce((acc, cur) => acc + cur, 0).toString();
-    }
+		return distances.reduce((acc, cur) => acc + cur, 0).toString();
+	}
 
-    solvedPuzzleTwo = true;
-    getSolutionTwo(file: string): string {
-        const [left, right] = this.processFile(file);
+	solvedPuzzleTwo = true;
+	getSolutionTwo(file: string): string {
+		const [left, right] = this.processFile(file);
 
-        // Create a map of the number of times the
-        // left list numbers appear in the right list
-        const map = new Map<number, number>(
-            left.map((number) => {
-                return [number, (right.filter((right) => right === number) ?? []).length];
-            })
-        );
+		// Create a map of the number of times the
+		// left list numbers appear in the right list
+		const map = new Map<number, number>(
+			left.map((number) => {
+				return [number, (right.filter((right) => right === number) ?? []).length];
+			}),
+		);
 
-        return [...map.entries()].reduce((acc, [num, cur]) => acc + num * cur, 0).toString();
-    }
+		return [...map.entries()].reduce((acc, [num, cur]) => acc + num * cur, 0).toString();
+	}
 }
--- src/core/run.ts
+++ src/core/run.ts
@@ -1,57 +1,57 @@
-import { bold, yellow } from "kolorist";
-import { exists, readFile } from "node:fs/promises";
-import { join } from "node:path";
-import { formatForAdvent } from "./format/for-advent";
+import { bold, yellow } from 'kolorist';
+import { exists, readFile } from 'node:fs/promises';
+import { join } from 'node:path';
+import { formatForAdvent } from './format/for-advent';
 
 export abstract class Run {
-    readonly year: string;
-    readonly day: string;
+	readonly year: string;
+	readonly day: string;
 
-    constructor(year: string, day: string) {
-        this.year = year;
-        this.day = day.replace(".ts", "").padStart(2, "0");
+	constructor(year: string, day: string) {
+		this.year = year;
+		this.day = day.replace('.ts', '').padStart(2, '0');
 
-        // check if all input files exist
-        this.assertInputFiles();
-    }
+		// check if all input files exist
+		this.assertInputFiles();
+	}
 
-    private async assertInputFiles() {
-        await Promise.all(
-            [1].map(async (solution) => {
-                const file = this.getFileNameForSolution(solution);
-                if (!(await exists(file))) {
-                    console.log(bold(yellow(`WARNING: Missing input file for solution ${solution}`)));
-                }
-            })
-        );
-    }
+	private async assertInputFiles() {
+		await Promise.all(
+			[1].map(async (solution) => {
+				const file = this.getFileNameForSolution(solution);
+				if (!(await exists(file))) {
+					console.log(bold(yellow(`WARNING: Missing input file for solution ${solution}`)));
+				}
+			}),
+		);
+	}
 
-    private getFileNameForSolution(solution: number) {
-        return join(process.cwd(), "src", this.year, "inputs", this.day, "input.txt");
-    }
+	private getFileNameForSolution(solution: number) {
+		return join(process.cwd(), 'src', this.year, 'inputs', this.day, 'input.txt');
+	}
 
-    private async getFileContent(solution: number) {
-        try {
-            return await readFile(this.getFileNameForSolution(solution), "utf-8");
-        } catch (e) {
-            throw new Error("No input file found.");
-        }
-    }
+	private async getFileContent(solution: number) {
+		try {
+			return await readFile(this.getFileNameForSolution(solution), 'utf-8');
+		} catch (e) {
+			throw new Error('No input file found.');
+		}
+	}
 
-    async run() {
-        console.clear();
-        console.log(bold(formatForAdvent(`Solution for AOC: [${this.year}]:[${this.day}]`)), "\n");
-        console.log(bold("Solution 1:"));
-        console.log(await this.getSolutionOne(await this.getFileContent(1)), "\n");
-        console.log(bold("Solution 2:"));
-        console.log(await this.getSolutionTwo(await this.getFileContent(2)), "\n");
-    }
+	async run() {
+		console.clear();
+		console.log(bold(formatForAdvent(`Solution for AOC: [${this.year}]:[${this.day}]`)), '\n');
+		console.log(bold('Solution 1:'));
+		console.log(await this.getSolutionOne(await this.getFileContent(1)), '\n');
+		console.log(bold('Solution 2:'));
+		console.log(await this.getSolutionTwo(await this.getFileContent(2)), '\n');
+	}
 
-    // To be implement per day
-    abstract getSolutionOne(file: string): Promise<string> | string;
-    abstract getSolutionTwo(file: string): Promise<string> | string;
+	// To be implement per day
+	abstract getSolutionOne(file: string): Promise<string> | string;
+	abstract getSolutionTwo(file: string): Promise<string> | string;
 
-    // Number to set solved
-    solvedPuzzleOne = false;
-    solvedPuzzleTwo = false;
+	// Number to set solved
+	solvedPuzzleOne = false;
+	solvedPuzzleTwo = false;
 }
