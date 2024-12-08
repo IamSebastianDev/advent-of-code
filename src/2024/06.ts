d76fc6299381a0be0fc09287c5958a30d24b3fd1
Sebastian Heinz <sebastian@iamsebastian.dev>
2024-12-08 10:34:02 +0100

    feat: day 6 solution one

--- src/2024/06.ts
+++ src/2024/06.ts
@@ -1,63 +1,109 @@
-import { red } from "kolorist";
-import { Files } from "../core/files";
-import { Run } from "../core/run";
+import { red } from 'kolorist';
+import { Files } from '../core/files';
+import { Run } from '../core/run';
 
 export default class extends Run {
-  private symbols: Record<string, "CLEAR" | "OBSTACLE"> = {
-    ".": "CLEAR",
-    "#": "OBSTACLE",
-  };
+	private symbols: Record<string, 'CLEAR' | 'OBSTACLE'> = {
+		'.': 'CLEAR',
+		'#': 'OBSTACLE',
+	};
 
-  private directions: Record<string, [number, number]> = {
-    "^": [0, -1],
-    ">": [1, 0],
-    "<": [-1, 0],
-    v: [0, 1],
-  };
+	private directions: Record<string, [number, number]> = {
+		'^': [0, -1],
+		'>': [1, 0],
+		v: [0, 1],
+		'<': [-1, 0],
+	};
 
-  private processFile(file: string) {
-    const lines = Files.getLines(file);
+	private processFile(file: string) {
+		const lines = Files.getLines(file);
 
-    // convert the input to a coordinate map
-    const map = new Map<string, string>([
-      ...lines.flatMap((line, yIdx) =>
-        line
-          .split("")
-          // This might be a windows specific thingy
-          .filter((char) => char !== "\r")
-          .map((char, xIdx, array) => {
-            return [`${xIdx}:${yIdx}`, this.symbols[char] ?? char] as const;
-          })
-      ),
-    ]);
+		// convert the input to a coordinate map
+		const map = new Map<string, string>([
+			...lines.flatMap((line, yIdx) =>
+				line
+					.split('')
+					// This might be a windows specific thingy
+					.filter((char) => char !== '\r')
+					.map((char, xIdx, array) => {
+						return [`${xIdx}:${yIdx}`, this.symbols[char] ?? char] as const;
+					}),
+			),
+		]);
 
-    return map;
-  }
+		return map;
+	}
 
-  solvedPuzzleOne = false;
-  getSolutionOne(file: string): string {
-    const map = this.processFile(file);
+	private getNextPosition(current: [number, number], direction: [number, number]) {
+		const next = [current[0] + direction[0], current[1] + direction[1]];
+		const key = `${next[0]}:${next[1]}`;
 
-    const origin = [...map.entries()].find(([_, entry]) =>
-      Object.keys(this.directions).includes(entry)
-    );
+		return key;
+	}
 
-    if (!origin) {
-      console.log(red("No origin found"));
-      process.exit(0);
-    }
+	private convertToPosition(pos: string) {
+		return pos.split(':').map((v) => Number.parseInt(v)) as [number, number];
+	}
 
-    const [startPosition, char] = origin;
-    const startDirection = this.directions[char];
-    const navigatedCells: string[] = [];
+	private getNextDirection([curX, curY]: [number, number]) {
+		const idx = Object.entries(this.directions).findIndex(([_, [x, y]]) => x === curX && y === curY);
+		if (idx === -1) {
+			throw Error('Direction not valid');
+		}
 
-    console.log({ startDirection, startPosition });
+		if (idx === 3) {
+			return Object.values(this.directions)[0];
+		}
 
-    return "";
-  }
+		return Object.values(this.directions)[idx + 1];
+	}
 
-  solvedPuzzleTwo = false;
-  getSolutionTwo(file: string): string {
-    return "";
-  }
+	solvedPuzzleOne = true;
+	getSolutionOne(file: string): string {
+		const map = this.processFile(file);
+
+		const origin = [...map.entries()].find(([_, entry]) => Object.keys(this.directions).includes(entry));
+
+		if (!origin) {
+			console.log(red('No origin found'));
+			process.exit(0);
+		}
+
+		const [startPosition, char] = origin;
+		const navigatedCells = new Set<string>(startPosition);
+
+		let currentPosition = this.convertToPosition(startPosition);
+		let currentDirection = this.directions[char];
+
+		const next = () => {
+			const nextPosition = this.getNextPosition(currentPosition, currentDirection);
+			const type = map.get(nextPosition);
+
+			if (!type) {
+				return false;
+			}
+
+			if (type === 'CLEAR' || Object.keys(this.directions).includes(type)) {
+				currentPosition = this.convertToPosition(nextPosition);
+				navigatedCells.add(nextPosition);
+			}
+
+			if (type === 'OBSTACLE') {
+				currentDirection = this.getNextDirection(currentDirection);
+			}
+
+			return true;
+		};
+
+		while (next()) {
+			// console.log(navigatedCells);
+		}
+
+		return navigatedCells.size.toString();
+	}
+
+	solvedPuzzleTwo = false;
+	getSolutionTwo(file: string): string {
+		return '';
+	}
 }
